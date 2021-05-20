171a551d6078bb8990640ad1b3a98d8c00ccd5d8
Mikhail <8916misha@gmail.com>
2021-05-20 18:08:53 +0300

    upd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QFileDialog
+import openpyxl
 
 from form import Ui_MainWindow  # импорт нашего сгенерированного файла
 import sys
@@ -14,9 +15,19 @@ class mywindow(QtWidgets.QMainWindow):
         self.ui.pushButton_2.clicked.connect(self.btnclicked)
 
     def btnclicked(self):
-        filename = QFileDialog.getOpenFileName(None, 'Открыть', os.path.dirname("C:\\"), 'All Files(*.xls)')
-        print(filename)
+        filename = QFileDialog.getOpenFileName(None, 'Открыть', os.path.dirname("C:\\"), 'All Files(*.xlsx)')
+        wb = openpyxl.load_workbook(filename[0])
+        # print(wb.get_sheet_names()[2])
+        sheet = (wb.get_sheet_by_name(wb.get_sheet_names()[2]))
+        massive = []
+        for x in range(1,9,1):
+            print(massive)
+            massive.append([])
+            print(massive[x-1])
+            for y in range(1,24,1):
+                massive[x-1].append(sheet.cell(row=x,column=y).value)
 
+        print(massive)
 
 
 
